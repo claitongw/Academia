2ada7f22e35f2ee744cadd99bcda1c6d318d4400
Claiton <cla.cgw@gmail.com>
2015-08-20 13:04:23 -0300

    Enviar para o github

---
+++ src/ManterServicos.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class ManterServicos {
+	int codDescricao;
+	String nomeDescricao;
+	ManterServicos[] arrayServicos = new ManterServicos[15];
+	
+	
+	void incluirServicos(int cDescricao, String nDescricao){
+		this.arrayServicos[codDescricao].codDescricao=cDescricao;
+		this.arrayServicos[codDescricao].nomeDescricao=nDescricao;
+		 
+	}
+	
+
+}
