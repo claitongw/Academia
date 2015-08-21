91fba90b951a4c7df199b0b999fc764cb0b13c0e
Claiton <cla.cgw@gmail.com>
2015-08-21 07:36:11 -0300

    antes de pull

--- src/Academia.java
+++ src/Academia.java
@@ -3,7 +3,12 @@ public class Academia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		ManterServicos iServico = new ManterServicos();
+		
+				
+		iServico.incluirServicos(1,"Claiton");
+		
 	}
 
 }
--- src/ManterServicos.java
+++ src/ManterServicos.java
@@ -4,9 +4,10 @@ public class ManterServicos {
 	ManterServicos[] arrayServicos = new ManterServicos[15];
 	
 	
-	void incluirServicos(int cDescricao, String nDescricao){
-		this.arrayServicos[codDescricao].codDescricao=cDescricao;
-		this.arrayServicos[codDescricao].nomeDescricao=nDescricao;
+	void incluirServicos(int codDescricao, String nomeDescricao){
+		arrayServicos[codDescricao].codDescricao=codDescricao;
+		arrayServicos[codDescricao].nomeDescricao=nomeDescricao;
+		System.out.println(nomeDescricao+" Incluido com sucesso!");
 	}
 	
 
