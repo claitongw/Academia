76875545679b4d4ae0627794b599cbf66fb49f15
Claiton Gilnei Watthier <cla.cgw@gmail.com>
2015-08-22 10:32:13 -0300

    Incluir/listar/alterar servicos OK.

--- src/ManterServicos.java
+++ src/ManterServicos.java
@@ -1,20 +1,43 @@
 public class ManterServicos {
 	public int codDescricao;
 	public String nomeDescricao;
-	
-	
 	ManterServicos[] arrayServicos = new ManterServicos[15];
+	int incremento=0;
 	
-
-
 	public void incluirServicos(int cDesc, String nDesc){
-		int incremento=0;
 		arrayServicos[incremento] = new ManterServicos();
 		arrayServicos[incremento].codDescricao=cDesc;
 		arrayServicos[incremento].nomeDescricao=nDesc;
-		System.out.println("Gravado!\nCód: "+cDesc+"|"+nDesc);
-		incremento++;
+		incremento=incremento+1;
 	}
 	
+	public void alterarServicos(int cDescAlterar, int cDescNovo, String nDescNovo){
+		int cont=0;
+		for(int x=0;arrayServicos[x]!=null;x++){
+			if(arrayServicos[x].codDescricao==cDescAlterar){
+				arrayServicos[x].codDescricao=cDescNovo;
+				arrayServicos[x].nomeDescricao=nDescNovo;
+				cont=cont+1;
+				System.out.println("SERVIÇO ALTERADO COM SUCESSO!");
+			}	
+		}
+		if(cont==0)
+			System.out.println("O CÓDIGO INFORMADO NÃO EXISTE!");
+	}
+	
+	public void excluirServicos(int cDescExcluir){
+		for(int x=0;arrayServicos[x]!=null;x++){
+			if(arrayServicos[x].codDescricao==cDescExcluir){
+				arrayServicos[x]=arrayServicos[x+1]; 
+				System.out.println("SERVIÇO EXCLUIDO COM SUCESSO!");
+			}
+		}
+	}
+	
+	public void listarServicos(){
+		for(int x=0;arrayServicos[x]!=null;x++){
+			System.out.println(arrayServicos[x].codDescricao+" | "+arrayServicos[x].nomeDescricao);
+		}
+	}
 
 }
