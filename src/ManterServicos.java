0d9c2f70798c85edaa68d67767c5ab88c0cb4e36
Claiton <cla.cgw@gmail.com>
2015-08-28 17:51:02 -0300

    a

--- src/ManterServicos.java
+++ src/ManterServicos.java
@@ -3,14 +3,14 @@ public class ManterServicos {
 	public String nomeDescricao;
 	ManterServicos[] arrayServicos = new ManterServicos[999999];
 	int incremento=0;
-	
+	//------------------------------------------
 	public void incluirServicos(int c, String n){
 		arrayServicos[incremento] = new ManterServicos();
 		arrayServicos[incremento].codDescricao=c;
 		arrayServicos[incremento].nomeDescricao=n;
 		incremento=incremento+1;
 	}
-	
+	//------------------------------------------
 	public void alterarServicos(int cDescAlterar, int cDescNovo, String nDescNovo){
 		int cont=0;
 		for(int x=0;arrayServicos[x]!=null;x++){
@@ -24,7 +24,16 @@ public class ManterServicos {
 		if(cont==0)
 			System.out.println("O CÓDIGO INFORMADO NÃO EXISTE!");
 	}
-	
+	//------------------------------------------
+	public void consultarServicos(int cDescConsultar){//CONSULTA POR CÓDIGO DO SERVIÇO
+		for(int x=0;arrayServicos[x]!=null;x++){
+			if(arrayServicos[x].codDescricao==cDescConsultar){
+				System.out.println(arrayServicos[x].codDescricao+" | "+arrayServicos[x].nomeDescricao);
+			}
+		}
+		System.out.println("O CÓDIGO INFORMADO NÃO EXISTE!");
+	}
+	//------------------------------------------
 	public void excluirServicos(int cDescExcluir){		
 		for(int x=0;arrayServicos[x]!=null;x++){
 			if(arrayServicos[x].codDescricao==cDescExcluir){
@@ -33,12 +42,12 @@ public class ManterServicos {
 		}
 		System.out.println("SERVIÇO EXCLUIDO COM SUCESSO!");
 	}
-	 
+	//------------------------------------------
 	public void listarServicos(){
 		for(int x=0;arrayServicos[x]!=null;x++){
 			if(arrayServicos[x].codDescricao!=0)
 			System.out.println(arrayServicos[x].codDescricao+" | "+arrayServicos[x].nomeDescricao);
 		}
 	}
-
+	//------------------------------------------
 }
--- src/Menu.java
+++ src/Menu.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Menu {
 	Scanner lerEscolha = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
+	Scanner in2 = new Scanner(System.in);
 	ManterServicos mServico = new ManterServicos();
 	
 	void mostrarMenu(){
@@ -22,8 +24,6 @@ public class Menu {
 }
 	
 	void mostrarMenuServicos(){
-		Scanner in = new Scanner(System.in);
-		Scanner in2 = new Scanner(System.in);
 		System.out.println("Menu\\Serviços | Selecione uma opção: ");
 		System.out.println("1 = Incluir:");
 		System.out.println("2 = Alterar:");
@@ -42,7 +42,8 @@ public class Menu {
 				
 				mServico.incluirServicos(cDesc, nDesc);
 				break;
-			}case 2:{
+			}
+			case 2:{
 				System.out.println("Digite o código do evento que deseja alterar:");
 				int cDescAlterar = in.nextInt();
 				System.out.println("Digite o novo código:");
@@ -52,6 +53,15 @@ public class Menu {
 							
 				mServico.alterarServicos(cDescAlterar, cDescNovo, nDescNovo);
 				break;
+			}
+			case 3:{
+				System.out.println("Escolha o tipo de pesquisa: (1-Código | 2-Nome)");
+				esc = lerEscolha.nextInt();
+				if(esc==2){
+					System.out.println("Qual código deseja consultar:");
+					int cDescConsultar = in.nextInt();
+					mServico.consultarServicos(cDescConsultar);
+				}
 			}
 			
 			
