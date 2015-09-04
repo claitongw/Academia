6280f89bacb6865134689ee9c3e28f965046b555
Claiton Gilnei Watthier <cla.cgw@gmail.com>
2015-09-04 07:10:25 -0300

    sexta

--- src/ManterProfessor.java
+++ src/ManterProfessor.java
@@ -1,34 +1,32 @@
 import java.util.ArrayList;
 
 	class Atributos{
-		public 	int 	codigo;
-		public	String  nome;
-		public	String 	cpf;
-		public 	String 	telefone;
-		public	String 	email;
+		 	int 	codigo;
+			String  nome;
+			String 	cpf;
+		 	String 	telefone;
+			String 	email;
 		
 	}
 
-	class ManterProfessor{
-	
+	public class ManterProfessor{
 
-
-
-	//public static Mensagens msg = new Mensagens();
+		//public static Mensagens msg = new Mensagens();
 	
 	void incluirProfessores(int codigo, String nome, String cpf, String telefone, String email){	//nome, cpf, telefone, email
+	
 		ArrayList<Atributos> arrayProfessores = new ArrayList<Atributos>();
 		Atributos adiciona = new Atributos();
 		
-		
 		adiciona.codigo=codigo;
-		adiciona.nome="Claiton";
+		adiciona.nome=nome;
 		adiciona.cpf=cpf;
 		adiciona.telefone=telefone;
 		adiciona.email=email;
 		
 		arrayProfessores.add(adiciona);
-		System.out.println(arrayProfessores.get(0));
+		System.out.println( arrayProfessores );
+		
 		//	msg.mensagemPopUp("Professor "+adiciona.nome+" cadastrado com sucesso!");
 		
 	}
